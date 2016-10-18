f8e75b1917df6b8d85c199045934192f0f7c75a4
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-10-18 23:51:36 +0200

    Cambiado controller para que devuelva el tipo correcto cuando se piden los usuarios

--- src/main/java/es/upm/miw/apiArchitectureTheme/controllers/UserController.java
+++ src/main/java/es/upm/miw/apiArchitectureTheme/controllers/UserController.java
@@ -1,5 +1,9 @@
 package es.upm.miw.apiArchitectureTheme.controllers;
 
+import java.util.List;
+
+import es.upm.miw.apiArchitectureTheme.daos.DaoFactory;
+import es.upm.miw.apiArchitectureTheme.entities.User;
 import es.upm.miw.apiArchitectureTheme.wrappers.SportWrapper;
 import es.upm.miw.apiArchitectureTheme.wrappers.UserListWrapper;
 import es.upm.miw.apiArchitectureTheme.wrappers.UserWrapper;
@@ -7,13 +11,20 @@ import es.upm.miw.apiArchitectureTheme.wrappers.UserWrapper;
 public class UserController {
 
 	public void createUser(UserWrapper user) {
+		DaoFactory.getFactory().getUserDao().create(new User(user.getNick(), user.getEmail()));
 	}
 
 	public void putSportToUser(UserWrapper userWrapper, SportWrapper sportWrapper) {
+		
 	}
 
 	public UserListWrapper userList() {
-		return null;
+		List<User> users = DaoFactory.getFactory().getUserDao().findAll();
+		UserListWrapper userListWrapper = new UserListWrapper();
+		for(User user : users){
+			userListWrapper.add(new UserWrapper(user.getNick(), user.getEmail()));
+		}
+		return userListWrapper;
 	}
 
 }
