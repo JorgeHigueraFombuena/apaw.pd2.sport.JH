64d08d6ee3064770f886d04c8c25ad42cb55b545
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-10-18 22:58:15 +0200

    Parte resource y wrapper de usuario. Añadido esqueleto de controlador de usuario

--- src/main/java/es/upm/miw/apiArchitectureTheme/api/UserResource.java
+++ src/main/java/es/upm/miw/apiArchitectureTheme/api/UserResource.java
@@ -1,24 +1,32 @@
 package es.upm.miw.apiArchitectureTheme.api;
 
+import es.upm.miw.apiArchitectureTheme.controllers.UserController;
 import es.upm.miw.apiArchitectureTheme.exceptions.InvalidSportException;
 import es.upm.miw.apiArchitectureTheme.exceptions.NickAlreadyExistsException;
+import es.upm.miw.apiArchitectureTheme.wrappers.SportWrapper;
 import es.upm.miw.apiArchitectureTheme.wrappers.UserListWrapper;
+import es.upm.miw.apiArchitectureTheme.wrappers.UserWrapper;
 
 public class UserResource {
 
 	public UserListWrapper userList() {
-		// TODO Auto-generated method stub
-		return null;
+		return new UserController().userList();
 	}
 
 	public void createUser(String nick, String email) throws NickAlreadyExistsException {
-		// TODO Auto-generated method stub
-		
+		UserWrapper user = new UserWrapper(nick,email);
+		if (userList().contains(user)){
+			throw new NickAlreadyExistsException(nick, email);
+		}
+		new UserController().createUser(user);
 	}
 
-	public void addSport(String nick, String sport) throws InvalidSportException {
-		// TODO Auto-generated method stub
-		
+	public void putSportToUser(String nick, String sport) throws InvalidSportException {
+		if(!new SportResource().sportList().contains(new SportWrapper(sport))){
+			throw new InvalidSportException(sport);
+		}
+		new UserController().putSportToUser(new UserWrapper(nick,null),
+				new SportWrapper(sport));
 	}
 
 }
--- src/main/java/es/upm/miw/apiArchitectureTheme/wrappers/UserListWrapper.java
+++ src/main/java/es/upm/miw/apiArchitectureTheme/wrappers/UserListWrapper.java
@@ -1,5 +1,33 @@
 package es.upm.miw.apiArchitectureTheme.wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserListWrapper {
 
+	private List<UserWrapper> userList = new ArrayList<>();
+	
+	public UserListWrapper(){
+	}
+
+	public List<UserWrapper> getUserList() {
+		return userList;
+	}
+
+	public void add(UserWrapper user){
+		userList.add(user);
+	}
+	
+	public boolean contains(UserWrapper user){
+		return userList.contains(user);
+	}
+	
+	@Override
+	public String toString(){
+		String result = "{\"userList\":[";
+		for(UserWrapper userWrapper : userList){
+			result += userWrapper.toString() + ",";
+		}
+		return result.substring(0, result.length() - 1) + "]";
+	}
 }
